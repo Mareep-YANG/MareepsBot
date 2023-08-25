add2b11d906b0b99799d520053a2f67a55a224d6
Mareep <ly1093322955@163.com>
2023-08-25 18:31:31 +0800

    格式化代码

--- src/main/java/org/mareep/test/MessageListener.java
+++ src/main/java/org/mareep/test/MessageListener.java
@@ -10,9 +10,17 @@ public class MessageListener implements EventListener {
 
         @EventHandler
         public void onMessage(GroupMessageEvent event) {
-                if (event.group_id == 859562984 && event.raw_message.equals("test")) {
-                        send_msg send_msg = new send_msg("group",  event.group_id, "测试成功", false);
-                        send_msg.send_msg();
+                if (event.group_id == 337995320) {
+                        if (event.raw_message.contains("原神")) {
+                                send_msg send_msg = new send_msg("group", event.group_id, "原神,启动!", false);
+                                send_msg.send_msg();
+                        }
+                }
+                if (event.group_id == 1029427187) {
+                        if (event.raw_message.contains("AC")) {
+                                send_msg send_msg = new send_msg("group", event.group_id, "玩你妈AC , 玩原神去", false);
+                                send_msg.send_msg();
+                        }
                 }
         }
 }
