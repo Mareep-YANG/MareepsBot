640f36f265136cc5defe71e9ec419ce90eb9f659
Mareep <ly1093322955@163.com>
2024-01-22 18:43:18 +0800

    kit

--- src/main/java/org/mareep/Main.java
+++ src/main/java/org/mareep/Main.java
@@ -3,7 +3,7 @@ package org.mareep;
 import com.alibaba.fastjson.JSONObject;
 import org.mareep.client.wsClient;
 import org.mareep.command.CommandListener;
-import org.mareep.test.testCommandListener;
+
 
 import java.io.File;
 import java.io.IOException;
@@ -19,19 +19,19 @@ public class Main {
     public static HashMap<String , JSONObject> EchoPool = new HashMap<>()  ;
     public static EventManager eventManager;
     public static wsClient client;
-    public static void main(String[] args) throws URISyntaxException {
+
+    public static void init(String cfgPath) throws URISyntaxException {
         ConfigurationManager configManager;
         //自动创建配置文件
-        if (!new File("config.properties").exists()) {
+        if (!new File(cfgPath).exists()) {
             try {
-                new File("config.properties").createNewFile();
-                configManager = new ConfigurationManager("config.properties");
+                new File(cfgPath).createNewFile();
+                configManager = new ConfigurationManager(cfgPath);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        }
-        else {
-             configManager = new ConfigurationManager("config.properties");
+        } else {
+            configManager = new ConfigurationManager(cfgPath);
         }
         // mode 为 WebSocket , url -> WebSocket的url
 
@@ -47,26 +47,5 @@ public class Main {
 
         //事件管理系统
         eventManager = new EventManager();
-        List<String> group_id = new ArrayList<String>() {
-            {
-                add("833217638");
-                add("337995320");
-                add("859562984");
-                add("663520905");
-            }
-        };
-
-        new CommandListener(group_id);
-        eventManager.registerListener(new testCommandListener());
-
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            String stringValue = scanner.next();
-            if (stringValue.equalsIgnoreCase("exit")) {
-                client.close();
-                System.exit(0);
-            }
-        }
-
     }
 }
